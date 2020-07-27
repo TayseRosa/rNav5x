d137c39f9bcb229dc7920121b45093e1bd27fc14
Tayse Rosa <tayse_rosa@yahoo.com.br>
2020-07-27 10:09:48 -0300

    B7Web - RNav5: Organização das Telas

--- App.js
+++ App.js
@@ -1,26 +1,12 @@
-// In App.js in a new project
-
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
+import MainStack from './src/navigators/MainStack';
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <MainStack />
     </NavigationContainer>
   );
 }
